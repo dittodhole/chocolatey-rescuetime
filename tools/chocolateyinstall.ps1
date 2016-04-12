d33a1f42132988440d6080173f5c1a5aca211711
Andreas Niedermair <andreas@niedermair.name>
2016-04-12 16:20:57 +0200

    added basic installation

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'dashlane'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS'
+$url = 'https://www.rescuetime.com/installers/RescueTimeInstaller.exe'
+$checksum = '78a442c835336986b580a0d1dd9f2c886761fefa'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
