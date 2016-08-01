7e73ff53ac310ff06d105ca510a6c5a90c2e9033
Andreas Niedermair <andreas@niedermair.name>
2016-08-01 22:55:01 +0200

    version update

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS'
 $url = 'https://www.rescuetime.com/installers/RescueTimeInstaller.exe'
-$checksum = 'a964e8ec53bf912e4e400553c034dc9f1ec04013'
+$checksum = '0ef921cb08f7362b5d5e80442493af26c432177b'
 $checksumType = 'sha1'
 $validExitCodes = @(0)
 
